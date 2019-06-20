ae23940e8b1d40eb67c957cec569651f24a328f0
MeowDada <chang.hungjui1251@gmail.com>
2019-06-20 11:33:50 +0800

    Fixed the script of vector_test.c

--- Vector/vector_test.c
+++ Vector/vector_test.c
@@ -4,9 +4,32 @@
 
 int main(int argc, char **argv)
 {
-	vector_t *vector;
-	vector_init(vector);
-	printf("%d\n", vector_size(vector));
+	vector_t v;
+	vector_init(&v);
+	
+	int i = 0;
+	
+	vector_add(&v, "Bonjour");
+    vector_add(&v, "tout");
+    vector_add(&v, "le");
+    vector_add(&v, "monde");
+
+    for (i = 0; i < vector_size(&v); i++)
+        printf("%s ", (char *) vector_get(&v, i));
+    printf("\n");
+
+    vector_delete(&v, 3);
+    vector_delete(&v, 2);
+    vector_delete(&v, 1);
+
+    vector_set(&v, 0, "Hello");
+    vector_add(&v, "World");
+
+    for (i = 0; i < vector_size(&v); i++)
+        printf("%s ", (char *) vector_get(&v, i));
+    printf("\n");
+
+    vector_free(&v);
 	
 	return 0;
 }
